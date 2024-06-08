ee450c466c2b60cb3ac7fce66ef8377389f1ba4d
Juan Delgado <juan._jpd@hotmail.com>
2024-06-07 22:13:10 -0300

    version 2

--- st7793.c
+++ st7793.c
@@ -1,9 +1,9 @@
 /*
- * ST7793 LCD for Raspberry Pi Zero 2W in 8-bit mode
+ * ST7793 LCD for Raspberry Pi Model B rev2
  */
-
-#define BCM2708_PERI_BASE 0xFE000000 /* For RPI Zero 2W - for older models this may change */
-#define GPIO_BASE (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
+ 
+#define BCM2708_PERI_BASE        0x20000000 /* 0xFE000000 */ 
+#define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -19,15 +19,15 @@
 #include <linux/platform_device.h>
 #include <linux/uaccess.h>
 
-#define BLOCKSIZE (4 * 1024)
+#define BLOCKSIZE (4*1024)
 
 // GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
-#define INP_GPIO(g) *(gpio + ((g) / 10)) &= ~(7 << (((g) % 10) * 3))
-#define OUT_GPIO(g) *(gpio + ((g) / 10)) |= (1 << (((g) % 10) * 3))
-#define SET_GPIO_ALT(g, a) *(gpio + (((g) / 10))) |= (((a) <= 3 ? (a) + 4 : (a) == 4 ? 3 : 2) << (((g) % 10) * 3))
+#define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio+(((g)/10))) |= (((a)<=3?(a)+4:(a)==4?3:2)<<(((g)%10)*3))
 
-#define GPIO_SET *(gpio + 7)  // sets bits which are 1 ignores bits which are 0
-#define GPIO_CLR *(gpio + 10) // clears bits which are 1 ignores bits which are 0
+#define GPIO_SET *(gpio+7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+10) // clears bits which are 1 ignores bits which are 0
 
 // GPIO pins for data lines (8-bit mode)
 #define DATA0 4
@@ -46,26 +46,26 @@
 #define WR 23
 #define RESET 24
 
-#define ORIENTATION 0 // 0=LANDSCAPE, 1=PORTRAIT
+#define ORIENTATION 0 //0=LANDSCAPE 1=PORTRAIT  
 
-#define DISPLAY_WIDTH 240
-#define DISPLAY_HEIGHT 400
+#define DISPLAY_WIDTH   240
+#define DISPLAY_HEIGHT  400
 
-#define DISPLAY_BPP 16
+#define DISPLAY_BPP     16
 
 volatile unsigned *gpio;
 
 // Set to output
 static void gpio_setoutput(char g)
 {
-    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO
+    INP_GPIO(g); // must use INP_GPIO before we can use OUT_GPIO 
     OUT_GPIO(g);
 }
 
 // Set state 1=high 0=low
-static void gpio_setstate(char g, char state)
+static void gpio_setstate(char g,char state)
 {
-    (state) ? (GPIO_SET = 1 << g) : (GPIO_CLR = 1 << g);
+    (state) ? (GPIO_SET = 1<<g) : (GPIO_CLR = 1<<g) ; 
 }
 
 // initialization of GPIO
@@ -86,59 +86,59 @@ static void tft_init_board(struct fb_info *info)
     gpio_setoutput(WR);
     gpio_setoutput(RESET);
     
-    gpio_setstate(DATA0, 0);
-    gpio_setstate(DATA1, 0);
-    gpio_setstate(DATA2, 0);
-    gpio_setstate(DATA3, 0);
-    gpio_setstate(DATA4, 0);
-    gpio_setstate(DATA5, 0);
-    gpio_setstate(DATA6, 0);
-    gpio_setstate(DATA7, 0);
+    gpio_setstate(DATA0,0);
+    gpio_setstate(DATA1,0);
+    gpio_setstate(DATA2,0);
+    gpio_setstate(DATA3,0);
+    gpio_setstate(DATA4,0);
+    gpio_setstate(DATA5,0);
+    gpio_setstate(DATA6,0);
+    gpio_setstate(DATA7,0);
     
-    gpio_setstate(DC, 1);
-    gpio_setstate(CS, 0);
-    gpio_setstate(RD, 1);
-    gpio_setstate(WR, 1);
-    gpio_setstate(RESET, 1);
+    gpio_setstate(DC,1);
+    gpio_setstate(CS,0);
+    gpio_setstate(RD,1);
+    gpio_setstate(WR,1);
+    gpio_setstate(RESET,1);
 }
 
 // hard reset of the graphic controller and the tft
 static void tft_hard_reset(void)
 {
-    gpio_setstate(RESET, 0);
+    gpio_setstate(RESET,0);
     msleep(120);
-    gpio_setstate(RESET, 1);
+    gpio_setstate(RESET,1);
     msleep(120);
 }
 
-static void gpio_set_parallel_data(unsigned char data)
+static void gpio_set_parallel_data(char data)
 {
-    gpio_setstate(DATA0, ((data >> 0) & 0x01));
-    gpio_setstate(DATA1, ((data >> 1) & 0x01));
-    gpio_setstate(DATA2, ((data >> 2) & 0x01));
-    gpio_setstate(DATA3, ((data >> 3) & 0x01));
-    gpio_setstate(DATA4, ((data >> 4) & 0x01));
-    gpio_setstate(DATA5, ((data >> 5) & 0x01));
-    gpio_setstate(DATA6, ((data >> 6) & 0x01));
-    gpio_setstate(DATA7, ((data >> 7) & 0x01));
+    gpio_setstate(DATA0,((data >> 0)  & 0x01));
+    gpio_setstate(DATA1,((data >> 1)  & 0x01));
+    gpio_setstate(DATA2,((data >> 2)  & 0x01));
+    gpio_setstate(DATA3,((data >> 3)  & 0x01));
+    gpio_setstate(DATA4,((data >> 4)  & 0x01));
+    gpio_setstate(DATA5,((data >> 5)  & 0x01));
+    gpio_setstate(DATA6,((data >> 6)  & 0x01));
+    gpio_setstate(DATA7,((data >> 7)  & 0x01));
 }
 
 // write command
-static void tft_command_write(unsigned char command)
+static void tft_command_write(char command)
 {
-    gpio_setstate(DC, 0);
+    gpio_setstate(DC,0);
     gpio_set_parallel_data(command);
-    gpio_setstate(WR, 0);
-    gpio_setstate(WR, 1);
+    gpio_setstate(WR,0);
+    gpio_setstate(WR,1);
 }
 
 // write data
-static void tft_data_write(unsigned char data)
+static void tft_data_write(char data)
 {
-    gpio_setstate(DC, 1);
+    gpio_setstate(DC,1);
     gpio_set_parallel_data(data);
-    gpio_setstate(WR, 0);
-    gpio_setstate(WR, 1);
+    gpio_setstate(WR,0);
+    gpio_setstate(WR,1);
 }
 
 // initialization of ST7793
@@ -151,13 +151,12 @@ static void tft_init(struct fb_info *info)
     msleep(120);
     tft_command_write(0x11); // Sleep out
     msleep(120);
+    tft_command_write(0x29); // Display on
     
     tft_command_write(0x3A); // Interface pixel format
-    tft_data_write(0x55);    // 16-bit pixel format (5-6-5 RGB)
+    tft_data_write(0x55);    // 16-bit pixel format
 
     // Add more initialization commands as needed based on the ST7793 datasheet
-
-    tft_command_write(0x29); // Display on
 }
 
 // write memory to TFT
@@ -173,7 +172,6 @@ static void st7793_update_display_area(const struct fb_image *image)
     
     tft_data_write((image->dx + image->width) >> 8);
     tft_data_write(image->dx + image->width);
-    
     // set row
     (ORIENTATION) ? tft_command_write(0x2A) : tft_command_write(0x2B);
     
@@ -380,49 +378,50 @@ static void st7793_deferred_io(struct fb_info *info, struct list_head *pagelist)
 }
 
 static struct fb_fix_screeninfo st7793_fix = {
-    .id = "st7793",
-    .type = FB_TYPE_PACKED_PIXELS,
-    .visual = FB_VISUAL_TRUECOLOR,
-    .accel = FB_ACCEL_NONE,
-    .xpanstep = 0,
-    .ypanstep = 0,
-    .ywrapstep = 0,
-    .line_length = DISPLAY_WIDTH * DISPLAY_BPP / 8,
+    .id             = "st7793",
+    .type           = FB_TYPE_PACKED_PIXELS,
+    .visual         = FB_VISUAL_TRUECOLOR,
+    .accel          = FB_ACCEL_NONE,
+    .xpanstep       = 0,
+    .ypanstep       = 0,
+    .ywrapstep      = 0,
+    .line_length    = DISPLAY_WIDTH * DISPLAY_BPP / 8,
 };
 
 static struct fb_var_screeninfo st7793_var = {
-    .width = DISPLAY_WIDTH,
-    .height = DISPLAY_HEIGHT,
+    .width          = DISPLAY_WIDTH,
+    .height         = DISPLAY_HEIGHT,
     .bits_per_pixel = DISPLAY_BPP,
-    .xres = DISPLAY_WIDTH,
-    .yres = DISPLAY_HEIGHT,
-    .xres_virtual = DISPLAY_WIDTH,
-    .yres_virtual = DISPLAY_HEIGHT,
-    .activate = FB_ACTIVATE_NOW,
-    .vmode = FB_VMODE_NONINTERLACED,
-    .nonstd = 0,
-    .red.offset = 11,
-    .red.length = 5,
-    .green.offset = 5,
-    .green.length = 6,
-    .blue.offset = 0,
-    .blue.length = 5,
-    .transp.offset = 0,
-    .transp.length = 0,
+    .xres           = DISPLAY_WIDTH,
+    .yres           = DISPLAY_HEIGHT,
+    .xres_virtual   = DISPLAY_WIDTH,
+    .yres_virtual   = DISPLAY_HEIGHT,
+    .activate       = FB_ACTIVATE_NOW,
+    .vmode          = FB_VMODE_NONINTERLACED,
+
+    .nonstd         = 0,
+    .red.offset     = 11,
+    .red.length     = 5,
+    .green.offset   = 5,
+    .green.length   = 6,
+    .blue.offset    = 0,
+    .blue.length    = 5,
+    .transp.offset  = 0,
+    .transp.length  = 0,
 };
 
 static struct fb_ops st7793_ops = {
-    .owner = THIS_MODULE,
-    .fb_read = st7793_read,
-    .fb_write = st7793_write,
-    .fb_fillrect = st7793_fillrect,
-    .fb_copyarea = st7793_copyarea,
-    .fb_imageblit = st7793_imageblit,
+    .owner          = THIS_MODULE,
+    .fb_read        = st7793_read,
+    .fb_write       = st7793_write,
+    .fb_fillrect    = st7793_fillrect,
+    .fb_copyarea    = st7793_copyarea,
+    .fb_imageblit   = st7793_imageblit,
 };
 
 static struct fb_deferred_io st7793_defio = {
-    .delay = HZ / 25,
-    .deferred_io = st7793_deferred_io,
+    .delay          = HZ/25,
+    .deferred_io    = st7793_deferred_io,
 };
 
 static unsigned int fps;
@@ -434,7 +433,7 @@ static int st7793_probe(struct platform_device *pdev)
     int vmem_size;
     unsigned char *vmem;
 
-    vmem_size = st7793_var.width * st7793_var.height * st7793_var.bits_per_pixel / 8;
+    vmem_size = st7793_var.width * st7793_var.height * st7793_var.bits_per_pixel/8;
     vmem = vzalloc(vmem_size);
     if (!vmem) {
         return -ENOMEM;
@@ -447,15 +446,19 @@ static int st7793_probe(struct platform_device *pdev)
         return -ENOMEM;
     }
 
-    info->screen_base = (char __force __iomem *)vmem;
+    info->screen_base = (char __force __iomem*)vmem;
     info->fbops = &st7793_ops;
     info->fix = st7793_fix;
     info->fix.smem_start = (unsigned long)vmem;
     info->fix.smem_len = vmem_size;
     info->var = st7793_var;
-    info->flags = FBINFO_VIRTFB;
+    info->flags = FBINFO_DEFAULT | FBINFO_VIRTFB;
 
     info->fbdefio = &st7793_defio;
+    if (0 < fps) {
+        info->fbdefio->delay = HZ/fps;
+    }
+
     fb_deferred_io_init(info);
 
     retval = register_framebuffer(info);
@@ -468,10 +471,6 @@ static int st7793_probe(struct platform_device *pdev)
     platform_set_drvdata(pdev, info);
 
     gpio = ioremap(GPIO_BASE, BLOCKSIZE);
-    if (!gpio) {
-        printk(KERN_ERR "Failed to map GPIO memory\n");
-        return -ENOMEM;
-    }
 
     tft_init_board(info);
     tft_hard_reset();
@@ -496,10 +495,10 @@ static int st7793_remove(struct platform_device *dev)
 }
 
 static struct platform_driver st7793_driver = {
-    .probe = st7793_probe,
+    .probe  = st7793_probe,
     .remove = st7793_remove,
     .driver = {
-        .name = "st7793",
+        .name   = "st7793",
     },
 };
 
